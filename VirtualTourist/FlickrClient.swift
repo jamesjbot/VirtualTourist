b2a5d5b76e9b1485ec0eba52dd8619efb0e366d6
jamesjbot <jongs.j@gmail.com>
2016-09-13 18:34:39 -0400

    Beginning to write basic flckr search code

--- VirtualTourist/FlickrClient.swift
+++ VirtualTourist/FlickrClient.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class FlickrClient {
     
@@ -22,16 +23,80 @@ class FlickrClient {
         return Singleton.sharedInstance
     }
     
+    
+    private func searchForPicturesByLatLon() {
+        let methodParameters = [
+            Constants.FlickrParameterKeys.Method : Constants.FlickrParameterValues.SearchMethod,
+            Constants.FlickrParameterKeys.APIKey : Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.SafeSearch : Constants.FlickrParameterValues.UseSafeSearch,
+            Constants.FlickrParameterKeys.Extras : Constants.FlickrParameterValues.MediumURL,
+            
+        ]
+    }
+    
+    private func createURLFromParameters(paramenters: [String:AnyObject]) -> NSURL {
+        let components = NSURLComponents()
+        components.scheme = Constants.Flickr.APIScheme
+        components.host = Constants.Flickr.APIHost
+        components.path = Constants.Flickr.APIPath
+        components.queryItems = [NSURLQueryItem]()
+        
+        for (key, value) in paramenters {
+            let queryItem = NSURLQueryItem(name: key, value: "\(value)")
+            components.queryItems?.append(queryItem)
+        }
+        return components.URL!
+    }
+    
+    func asyncLoadImage( completion: (photo: UIImage, error: NSError?) -> Void) {
+        
+    }
+    
+    
 }
 
 
 extension FlickrClient {
     struct Constants {
+        // MARK: Flickr
         struct Flickr {
             static let APIScheme = "https"
             static let APIHost = "api.flickr.com"
             static let APIPath = "/services/rest"
-            static let apiKey = "cf3e089c7821258a56d0dfc84b14a4a2"
+            
+        }
+        
+        // MARK: Flickr Parameter Keys
+        struct FlickrParameterKeys {
+            static let Method = "method"
+            static let APIKey = "api_key"
+            static let Extras = "extras"
+            static let SafeSearch = "safe_search"
+            static let Format = "format"
+            static let NoJSONCallback = "nojsoncallback"
+        }
+        
+        // MARK: Flickr Parameter Values
+        struct FlickrParameterValues {
+            static let SearchMethod = "flickr.photos.search"
+            static let APIKey = "cf3e089c7821258a56d0dfc84b14a4a2"
+            static let MediumURL = "medium_url"
+            static let UseSafeSearch = "1"
+            static let ResponseFormat = "json"
+            static let DisableJSONCallback = "1"
+        }
+        
+        // MARK: Flickr Response Keys
+        struct FlickrResponseKeys {
+            static let Status = "stat"
+            static let Photo = "photos"
+            static let MediumURL = "url_m"
+            static let Total = "total"
+        }
+        
+        // MARK: Flickr Response Values
+        struct FlickrRespnseValues {
+            static let OKStatus = "ok"
         }
     }
 }
