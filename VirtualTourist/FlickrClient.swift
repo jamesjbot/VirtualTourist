275c147d0bd57bffcef5186e68d1fce654fdb9c1
jamesjbot <jongs.j@gmail.com>
2016-12-20 03:53:05 -0500

    Fix the processing of photos array. Swift conversion didn't quite get it right.

--- VirtualTourist/FlickrClient.swift
+++ VirtualTourist/FlickrClient.swift
@@ -95,8 +95,8 @@ class FlickrClient {
                     // Perform model updates
                     let photosElement = dict![Constants.FlickrResponseKeys.Photos]
                     print(photosElement)
-                    //self.photoSearchResultsArray = [photosElement as! [String:AnyObject]]
-                    //self.photoSearchResultsArray = (photosElement! as! [[String:AnyObject]])[Constants.FlickrResponseKeys.Photo]
+                    let photoResults = photosElement as! [String:AnyObject]
+                    self.photoSearchResultsArray = photoResults[Constants.FlickrResponseKeys.Photo] as! [[String : AnyObject]]
                     if let completionHandlerTopLevel = completionHandlerTopLevel {
                         completionHandlerTopLevel(true, photosElement as? Data, nil)
                     }
@@ -288,7 +288,7 @@ class FlickrClient {
     }
     
     fileprivate func getBackgroundContextFetchedResultsController() -> NSFetchedResultsController<Photo>? {
-        let request = NSFetchRequest<Photo>()//(entityName: "Photo")
+        let request = NSFetchRequest<Photo>(entityName: "Photo")
         request.sortDescriptors = []
         let p = NSPredicate(format: "pin = %@", argumentArray: [pinLocation!])
         request.predicate = p
@@ -301,68 +301,5 @@ class FlickrClient {
         return frc
     }
     
-}
-
-extension FlickrClient {
-    struct Constants {
-        // MARK: Flickr
-        struct Flickr {
-            static let APIScheme = "https"
-            static let APIHost = "api.flickr.com"
-            static let APIPath = "/services/rest"
-            static let ImageSize = "m"
-            static let MaximumShownImages = 21
-            
-            static let SearchBBoxHalfWidth = 1.0
-            static let SearchBBoxHalfHeight = 1.0
-            static let SearchLatRange = (-90.0, 90.0)
-            static let SearchLonRange = (-180.0, 180.0)
-        }
-        
-        // MARK: Flickr Parameter Keys
-        struct FlickrParameterKeys {
-            static let Method = "method"
-            static let APIKey = "api_key"
-            static let Extras = "extras"
-            static let SafeSearch = "safe_search"
-            static let Format = "format"
-            static let NoJSONCallback = "nojsoncallback"
-            static let Lat = "lat"
-            static let Lon = "lon"
-            static let BoundingBox = "bbox"
-        }
-        
-        // MARK: Flickr Parameter Values
-        struct FlickrParameterValues {
-            static let SearchMethod = "flickr.photos.search"
-            static let APIKey = "cf3e089c7821258a56d0dfc84b14a4a2"
-            static let MediumURL = "medium_url"
-            static let UseSafeSearch = "1"
-            static let ResponseFormat = "json"
-            static let DisableJSONCallback = "1"
-        }
-        
-        // MARK: Flickr Response Keys
-        struct FlickrResponseKeys {
-            static let Status = "stat"
-            static let Photos = "photos"
-            static let Photo = "photo"
-            static let MediumURL = "url_m"
-            static let Total = "total"
-        }
-        
-        // MARK: Flickr Response Values
-        struct FlickrRespnseValues {
-            static let OKStatus = "ok"
-        }
-        
-        // MARK: Flickr URL Construction
-        struct FlickrURLConstructValue {
-            static let Farm = "farm"
-            static let Server = "server"
-            static let Secret = "secret"
-            static let Id = "id"
-        }
-    }
 }
 
